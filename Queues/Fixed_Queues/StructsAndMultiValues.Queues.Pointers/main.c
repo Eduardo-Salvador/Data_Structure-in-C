a8bfd37d4921d788e656c7b2d3a65f5a42fdc6e0
Eduardo-Salvador <eduardo.salvador@gmail.com>
2025-09-21 23:53:24 -0300

    Feat: Fixed Queues - Pointers and correction of some functions

---
+++ Queues/Fixed_Queues/StructsAndMultiValues.Queues.Pointers/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include "fixedQueues.h"
+
+int main(){
+	int option;
+    Queue q;
+	Person p;
+	queue(&q);
+
+	do {
+		printf("\n\nMENU");
+        printf("\n1. Initialize");
+        printf("\n2. Insert");
+        printf("\n3. Remove");
+        printf("\n4. Print");
+        printf("\n5. First Element");
+        printf("\n6. Queue size");
+        printf("\n7. Exit");
+        printf("\nEnter the desired option: ");
+        scanf("%d", &option);
+		
+		switch(option) {
+			case 1:
+				queue(&q);
+				break;
+			case 2:
+                printf("Enter the name: ");
+				scanf(" %s", &p.name);
+				printf("Enter the age: ");
+				scanf(" %d", &p.age);
+				printf("Enter the gender (M ou W): ");
+				scanf(" %c", &p.gender);
+				while (p.gender != 'M' && p.gender != 'W') {
+					printf("Incorrect Gender. Enter again (M/W): ");
+					scanf(" %c", &p.gender); 
+				}
+				enqueue(&q, p);
+				break;
+			case 3:
+				dequeue(&q);
+				break;
+			case 4:
+				list(&q);
+				break;
+			case 5:
+				peek(&q);
+				break;
+            case 6:
+                size(&q);
+                break;
+			case 7:
+				printf("Ending the program...");
+				break;
+			default:
+				printf("\nInvalid option. Please choose a valid option number.");
+		}	
+	} while(option != 7);
+}
